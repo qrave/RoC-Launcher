664e9fd59fd7bd43599e3ad47b5c0165013a5b06
qrave <napokam@icloud.com>
2017-09-13 21:22:38 +0100

    Fixes for TC

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const fs = require('fs');
-var url = "https://github.com/dpwhittaker/RoC-Launcher/releases/download/Assets/relics_1.tre";
+var url = "http://www.swginfinity.com/updates/infinity_00.tre";
 var file = fs.createWriteStream("C:\\SWGTest\\relics_1.tre");
 request(url).on('error', err => {
     process.send("download error " + err);
